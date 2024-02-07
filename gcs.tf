65558d43558fefa0af2fb22d0466c2813dd4a93f
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-07 12:57:37 +0000

    code remove

--- gcs.tf
+++ gcs.tf
@@ -7,15 +7,8 @@ resource "google_storage_bucket" "bucket" {
   name                        = "ai-demo-bucket-${random_id.random.hex}"
   location                    = "EU"
   uniform_bucket_level_access = true
-  # versioning {
-  #   enabled = true
-  # }
-  # logging {
-  #   log_bucket        = "ai-demo-logs-bucket-${random_id.random.hex}"
-  #   log_object_prefix = "logs"
-  # }
 }
 
 resource "random_id" "random" {
-  byte_length = 3
+  byte_length = 4
 }
