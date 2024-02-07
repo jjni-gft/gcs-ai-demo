f5e850bcfbefdebe8a9f3d95efb5e2ae496f4eb2
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-07 12:47:29 +0000

    comment versioning

--- gcs.tf
+++ gcs.tf
@@ -7,9 +7,9 @@ resource "google_storage_bucket" "bucket" {
   name                        = "ai-demo-bucket-${random_id.random.hex}"
   location                    = "EU"
   uniform_bucket_level_access = true
-  versioning {
-    enabled = true
-  }
+  # versioning {
+  #   enabled = true
+  # }
   # logging {
   #   log_bucket        = "ai-demo-logs-bucket-${random_id.random.hex}"
   #   log_object_prefix = "logs"
