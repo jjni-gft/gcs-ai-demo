ba6887bc8e08b813e5b94655ca591ff9b767e32e
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-12 11:25:57 +0000

    location codefix

--- gcs.tf
+++ gcs.tf
@@ -1,11 +1,10 @@
 provider "google" {
   project = "dev-env-1-412811"
-  region  = "europe-west1"
 }
 
 resource "google_storage_bucket" "bucket" {
   name     = "my-bucket-1672"
-  location = "EU"
+
 
   uniform_bucket_level_access = true
 
