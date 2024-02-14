8222851d60733b6749fa9ede9576b7ca5c101f9b
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-14 12:25:24 +0000

    codefix bucket change

--- bucket.tf
+++ bucket.tf
@@ -1,20 +1,15 @@
 provider "google" {
   project = "dev-env-1-412811"
+  region  = "us-central1"
 }
 
 resource "random_id" "bucket_id" {
-  byte_length = 2
+  byte_length = 8
 }
 
 resource "google_storage_bucket" "bucket" {
-  name                        = "bucket-${random_id.bucket_id.hex}"
-  location                    = "US"
-  versioning {
-    enabled = true
-  }
-  logging {
-    log_bucket        = "bucket-${random_id.bucket_id.hex}-logs"
-    log_object_prefix = "log"
-  }
+  name     = "my-bucket-${random_id.bucket_id.hex}"
+  location = 
+
   uniform_bucket_level_access = true
 }
