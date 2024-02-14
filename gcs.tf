73dbf00d5d729171ef7c7427250fdc8c7f2b24c8
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-14 11:07:06 +0000

    folder name change codefix

---
+++ gcs.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = "dev-env-1-412811"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 2
+}
+
+resource "google_storage_bucket" "bucket" {
+  name                        = "bucket-${random_id.bucket_id.hex}"
+  location                    = "US"
+
+  uniform_bucket_level_access = true
+}
+#test
