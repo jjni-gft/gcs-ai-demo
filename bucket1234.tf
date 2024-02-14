13a2e53b5b017041cff41a008e0a43b705ec145c
Pasang-GFT <158459451+Pasang-GFT@users.noreply.github.com>
2024-02-14 14:28:07 +0000

    [GFTCodeFix]: Update on  bucket1234.tf (#160)

--- bucket1234.tf
+++ bucket1234.tf
@@ -8,8 +8,14 @@ resource "random_id" "bucket_id" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-${random_id.bucket_id.hex}"
-  location = 
-
+  name                        = "my-bucket-${random_id.bucket_id.hex}"
+  location                    = "us-central1"
   uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  logging {
+    log_bucket        = "my-logs-bucket"
+    log_object_prefix = "log"
+  }
 }
