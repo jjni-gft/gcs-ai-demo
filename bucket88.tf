81232b3b1d8e37532cc3ff6c9c4125dd17dd0021
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-15 10:02:55 +0000

    bucket create

--- bucket88.tf
+++ bucket88.tf
@@ -8,7 +8,7 @@ resource "random_id" "bucket_id" {
 
 resource "google_storage_bucket" "bucket" {
   name     = "my-bucket-${random_id.bucket_id.hex}"
-  location = 
+  location = "US"
 
   uniform_bucket_level_access = true
 }
