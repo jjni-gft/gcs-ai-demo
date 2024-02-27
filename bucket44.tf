be4fd938dd2f022f32b4c8d4a53d302fc1190178
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-27 10:46:57 +0000

    bucket delete

--- bucket44.tf
+++ bucket44.tf
@@ -3,12 +3,4 @@ provider "google" {
   region  = "us-central1"
 }
 
-resource "random_id" "bucket_id" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-${random_id.bucket_id.hex}"
-  location = "US"
-}
 
