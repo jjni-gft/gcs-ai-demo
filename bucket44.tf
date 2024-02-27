069247bbfdf2cc7179282b049c6181946ac596a6
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-02-27 12:53:57 +0000

    bucket creation

--- bucket44.tf
+++ bucket44.tf
@@ -3,3 +3,11 @@ provider "google" {
   region  = "us-central1"
 }
 
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "my-bucket-${random_id.bucket_id.hex}"
+  location = "US"
+}
