fe57196333105616969fbc133ee85f6efe0b18d2
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-03-13 12:59:45 +0000

    remove files for demo

--- bucket.tf
+++ bucket.tf
@@ -1,22 +1,4 @@
 provider "google" {
-  project = var.project
-  region  = var.region
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = var.byte_length
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-${random_id.bucket_id.hex}"
-  location = var.bucket_location
-}
-
-module "gcs_bucket" {
-  source  = "terraform-google-modules/cloud-storage/google"
-  version = "1.7.0"
-
-  project_id  = var.project
-  names = [var.bucket_name]
-  location    = var.bucket_location
+  project        = "dev-env-1-412811"
+  region         = "us-central1"
 }
