af5dc3b5f4325021f6e28860d9f9531eefa0def3
Google Checklist Cloud Build Service Account <Pasang.Sherpa@gft.com>
2024-03-13 13:29:01 +0000

    breaking change

--- bucket.tf
+++ bucket.tf
@@ -10,5 +10,5 @@ resource "random_id" "bucket_id" {
 resource "google_storage_bucket" "bucket" {
   name     = "my-bucket-${random_id.bucket_id.hex}"
   project  = "dev-env-1-412811"
-  location = "US"
+  location = 
 }
