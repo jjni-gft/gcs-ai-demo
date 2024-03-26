1839f46cda0d18c4b2151a42182b334062bad204
Pasanghh <86737740+Pasanghh@users.noreply.github.com>
2024-03-26 09:50:07 +0000

    bucket creation

--- bucket.tf
+++ bucket.tf
@@ -2,13 +2,3 @@ provider "google" {
   project        = "dev-env-1-412811"
   region         = "us-central1"
 }
-
-resource "random_id" "bucket_id_suffix" {
-  byte_length = 2
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = "my-bucket-${random_id.bucket_id_suffix.hex}"
-  project  = "dev-env-1-412811"
-  location = "US"
-}
